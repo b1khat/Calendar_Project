341f2cb9aa8892cabd9360c38e9b5cd16dea46a7
Uljaco <ulyssescollins95@gmail.com>
2015-10-15 17:37:56 -0500

    Update EventDriver.java

--- Button_Tutorial/CalendarSource/EventDriver.java
+++ Button_Tutorial/CalendarSource/EventDriver.java
@@ -7,15 +7,19 @@ public class EventDriver{
 		out.println(event1.getName());
 		out.println(event1.getStartTime());
 		out.println(event1.getEndTime());
+		out.println(event1.getCategory());
 		out.println(event1);
 
-		Event event2 = new Event("Birthday", new Date(1995-1900, 6, 27, 7, 30), new Date(1995-1900, 6, 27, 8, 42));
+		//Test Fixture
+		Category defaultCat = new Category("Default");
+
+		Event event2 = new Event("Birthday", new Date(1995-1900, 6, 27, 7, 30), new Date(1995-1900, 6, 27, 8, 42), defaultCat);
 		out.println(event2);
 
-		Event event3 = new Event("BirthdayBro", new Date(1995-1900, 6, 27, 7, 30), new Date(1995-1900, 6, 27, 8, 42), "Mesquite, TX");
+		Event event3 = new Event("BirthdayBro", new Date(1995-1900, 6, 27, 7, 30), new Date(1995-1900, 6, 27, 8, 42), defaultCat, "Mesquite, TX");
 		out.println(event3);
 
-		Event event4 = new Event("Birthday", new Date(1995-1900, 6, 27, 7, 30), new Date(1995-1900, 6, 27, 8, 42), "Masskqweet, Tejas", "Best time to be alive");
+		Event event4 = new Event("Birthday", new Date(1995-1900, 6, 27, 7, 30), new Date(1995-1900, 6, 27, 8, 42), defaultCat, "Masskqweet, Tejas", "Best time to be alive");
 		out.println(event4);
 
 	}
